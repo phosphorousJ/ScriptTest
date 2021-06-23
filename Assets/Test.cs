34ed0797976a38560f7cd724e5d5f5bd5575e203
phosphorousJ <86190512+phosphorousJ@users.noreply.github.com>
2021-06-23 21:44:29 +0900

    発展課題（Bossクラス）の削除
    配列の課題をするために、先にプログラムした発展課題を削除した。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,59 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss
-{
-    private int hp = 100;
-    private int power = 25;
-    public int mp = 53;        //MP用
-
-    public void Attack()
-    {
-        Debug.Log(this.power + "のダメージを与えた");
-    }
-
-    public void Defence(int damage)
-    {
-        Debug.Log(damage + "のダメージを受けた");
-        this.hp -= damage;
-    }
-
-    //魔法攻撃用の関数
-    public void Magic() 
-    {
-
-        //魔法攻撃を10回繰り返す
-        for(int i = 1; i <= 10; i++) 
-        {
-            if (mp >= 5) 
-            {
-                mp -= 5;
-                Debug.Log("魔法攻撃をした。残りのMPは" + mp + ".");
-            }
-        }
-
-        Debug.Log("MPが足りないため魔法が使えない。");
-    }
-}
-
-
 public class Test : MonoBehaviour
 {
     
     // Start is called before the first frame update
     void Start()
     {
-        Boss lastboss = new Boss();
-        lastboss.Attack();
-        lastboss.Defence(3);
+        int[] array = { 10, 76, 32, 89, 265 };
 
-        //魔法攻撃用の関数を呼び出す
-        lastboss.Magic();
+        //配列の各要素を順番に表示
+        for (int i = 0; i < array.Length; i++) 
+        {
+            Debug.Log(array[i]);
+        }
 
-
-        Boss midboss = new Boss();
-        midboss.Attack();
-        midboss.Defence(5);
+        //配列の各要素を逆順に表示
+        for (int j = 4; j >= 0 ; j--) 
+        {
+            Debug.Log(array[j]);
+        }
     }
 
     // Update is called once per frame
